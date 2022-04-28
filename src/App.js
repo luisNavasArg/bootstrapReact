575452329e75c2c31dae63dcde0c8e220ad5043d
luisNavasArg <lpipnavas@gmail.com>
2022-04-28 14:00:26 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col, Form, FormLabel, FormControl, FormGroup,Alert} from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary bg-opacity-50">
+      {['primary','secondary','success', 'danger', 'warning','info','light','dark'].map((a,i)=>{
+        return(
+          <Alert key={i+"ale"} variant={a}>Tenemos un aler con la variante {a}</Alert>
+        )
+      })}
+      <Container>
+        <Row>
+          <Col>
+            <Button variant='primary col-6 my-5'>Azul</Button>
+            <Button variant='secundary col-6'>Gris</Button>
+          </Col>
+          <Col>
+          <Button variant='primary col-6 my-5'>Azul</Button>
+          <Button variant='secundary col-6'>Gris</Button>
+          </Col>
+          <Col>
+            <Button variant='primary col-6 my-5'>Azul</Button>
+            <Button variant='secundary col-6'>Gris</Button>
+          </Col>
+          <Col>
+          <Button variant='primary col-6 my-5'>Azul</Button>
+          <Button variant='secundary col-6'>Gris</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Button variant='primary my-5'>Azul</Button>
+          <Button variant='secundary '>Gris</Button>
+        </Row>
+      </Container>
+      <Container className="row justify-content-center">
+        <Form className='w-50 bg-danger bg-opacity-25'>
+          <FormGroup className="my-5">
+          <FormLabel>Email</FormLabel>
+          <FormControl type="email" placeholder='Ingresa tu email'/>
+          </FormGroup>
+          <FormGroup className="my-5">
+          <FormLabel>Clave</FormLabel>
+          <FormControl type="password" placeholder='Ingresa tu clave'/>
+          </FormGroup>
+          
+        </Form>
+      </Container>
     </div>
   );
 }
